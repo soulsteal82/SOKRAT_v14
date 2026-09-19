375543b9cff5b9ece2764f849c4a5bceb8dba59d
soulsteal82 <soulsteal82@gmail.com>
2026-09-19 04:50:36 +0000

    Fix: remove webpack config for Turbopack compatibility

--- sokrat-app/next.config.ts
+++ sokrat-app/next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    if (isServer) {
-      // Prevent Leaflet from being bundled on the server
-      config.externals = [...(config.externals || []), 'leaflet', 'react-leaflet'];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
