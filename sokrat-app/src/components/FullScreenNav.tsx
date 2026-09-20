5e19e1dec13080563a56bfa09775cc152a46f544
soulsteal82 <soulsteal82@gmail.com>
2026-09-20 12:01:31 +0000

    feat: switch map tiles to Stadia Maps with API key

--- sokrat-app/src/components/FullScreenNav.tsx
+++ sokrat-app/src/components/FullScreenNav.tsx
@@ -273,10 +273,9 @@ export default function FullScreenNav({
             dragging={true}
             zoomControl={true}
           >
-             <TileLayer
-              attribution="&copy; OpenStreetMap contributors &copy; CARTO"
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-              subdomains={["a", "b", "c", "d"]}
+            <TileLayer
+              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+              url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_STADIA_API_KEY || ""}`}
             />
 
             {/* Route: colored traffic segments */}
--- sokrat-app/src/components/MiniMap.tsx
+++ sokrat-app/src/components/MiniMap.tsx
@@ -231,9 +231,8 @@ export default function MiniMap({
           zoomControl={true}
         >
           <TileLayer
-            attribution="&copy; OpenStreetMap contributors &copy; CARTO"
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-            subdomains={["a", "b", "c", "d"]}
+            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+            url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.NEXT_PUBLIC_STADIA_API_KEY || ""}`}
           />
 
           {/* Real OSRM road route, colored by traffic segments */}
